1a2bfdcf8671f11f71aa74e4df998767b676bbad
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-04 21:15:39 +0200

    Change Selection Colors & Added Effect When Clicking On Dice Button

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,7 +1,18 @@
 import dividerDesktop from "../images/pattern-divider-desktop.svg"
 import diceIcon from "../images/icon-dice.svg"
+import { useRef } from "react"
 
 export function Advice() {
+
+    const diceRef = useRef()
+
+    function diceClicked() {
+        diceRef.current.style.scale = "0.9 0.9"
+        setTimeout(() => {
+            diceRef.current.style.scale = 1 
+        }, 100);
+    }
+
   return (
     <div className="container rounded-2xl relative p-11 text-center mx-3 max-w-80 sm:max-w-lg">
       <h1 className="font-medium tracking-[.2em] text-sm">ADVICE #117</h1>
@@ -13,8 +24,11 @@ export function Advice() {
         <img src={dividerDesktop} alt="divder desktop" />
       </div>
       <div
-        className="dice absolute left-1/2 -bottom-7 -translate-x-1/2 flex 
-            justify-center content-center rounded-full p-5 cursor-pointer transition-all duration-300"
+        className="dice absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2 flex 
+            justify-center content-center rounded-full p-5 cursor-pointer 
+            transition-all duration-300 select-none"
+        ref={diceRef}
+        onClick={diceClicked}
       >
         <img src={diceIcon} alt="dice" />
       </div>
